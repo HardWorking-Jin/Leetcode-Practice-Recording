185fe859348a7b544eb87027ea89c02abfc83c56
HardWorking-Jin <90534122+HardWorking-Jin@users.noreply.github.com>
2021-11-18 14:15:54 -0800

    Update Leetcode704_BinarySearch.py

--- Array/Leetcode704_BinarySearch.py
+++ Array/Leetcode704_BinarySearch.py
@@ -6,13 +6,13 @@ class Solution(object):
         :rtype: int
         """
         left = 0
-        right = len(nums)-1
-        while left <= right:
+        right = len(nums)
+        while left < right:
             mid = (left+right)//2
             if target == nums[mid]:
                 return mid
             elif target < nums[mid]:
-                right = mid-1
+                right = mid
             elif target > nums[mid]:
                 left = mid+1
         return -1
