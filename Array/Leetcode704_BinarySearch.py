31bad6be38a63b20d5bad6dfb475a473ef731e5a
HardWorking-Jin <90534122+HardWorking-Jin@users.noreply.github.com>
2021-11-19 12:00:54 -0800

    Update Leetcode704_BinarySearch.py

--- Array/Leetcode704_BinarySearch.py
+++ Array/Leetcode704_BinarySearch.py
@@ -17,19 +17,18 @@ class Solution(object):
                 left = mid+1
         return -1
 
+sol = Solution()
 
 # Test Case1, the answer should be 4
 nums1 = [-1,0,3,5,9,12]
 target1 = 9
 
+answer1 = sol.search(nums1, target1)
+print(answer1)
+
 # Test Case2, the answer should be -1
 nums2 = [-1,0,3,5,9,12]
 target2 = 2
-
-sol = Solution()
-
-answer1 = sol.search(nums1, target1)
-print(answer1)
 
 answer2 = sol.search(nums2, target2)
 print(answer2)
