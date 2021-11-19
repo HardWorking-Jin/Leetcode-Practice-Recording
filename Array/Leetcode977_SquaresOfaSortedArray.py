32bd29b2368cd9245faed810bc47f529e79c9899
HardWorking-Jin <90534122+HardWorking-Jin@users.noreply.github.com>
2021-11-19 12:43:00 -0800

    Update Leetcode977_SquaresOfaSortedArray.py

--- Array/Leetcode977_SquaresOfaSortedArray.py
+++ Array/Leetcode977_SquaresOfaSortedArray.py
@@ -5,17 +5,26 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        ans = []
-        for n in nums:
-            ans.append(n**2)
-        
-        ans.sort()
+        leftPointer = 0
+        rightPointer = len(nums)-1
+        cur = len(nums)-1
+        ans = [0]*len(nums)
+
+        while leftPointer < rightPointer:
+            if abs(nums[leftPointer]) > abs(nums[rightPointer]):
+                ans[cur] = nums[leftPointer]**2
+                leftPointer = leftPointer+1
+            else:
+                ans[cur] = nums[rightPointer]**2
+                rightPointer = rightPointer-1
+            cur = cur-1
+
         
         return ans
 
 sol = Solution()
 
 #  Test Case, answer should be [0, 1, 9, 16, 100]
-nums1 = [-4, -1, 0, 3, 10]
+nums1 = [-6, -1, 2, 3, 10]
 ans1 = sol.sortedSquares(nums1)
 print(ans1)
