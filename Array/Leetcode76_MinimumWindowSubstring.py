f18319ed66a3a7c3671df627c3d122ab4cd8d342
HardWorking-Jin <90534122+HardWorking-Jin@users.noreply.github.com>
2021-11-20 22:48:27 -0800

    Update Leetcode76_MinimumWindowSubstring.py

--- Array/Leetcode76_MinimumWindowSubstring.py
+++ Array/Leetcode76_MinimumWindowSubstring.py
@@ -5,44 +5,51 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        left = 0
-        size = len(s)
-        new_s = {}
-        new_t = {}
-        formed = 0
-        res = [float('inf'), 0, 0]
         
+        new_t = {}
         for char in t:
             if char in new_t:
                 new_t[char] += 1
             else:
-                new_t[char] = 1    
+                new_t[char] = 1
+
+        new_s = []
+        for i, char in enumerate(s):
+            if char in new_t:
+                new_s.append([i, char])
+
+        window = {}
+        formed = 0
+        left = 0
+        res = [float('inf'), 0, 0]    
+        for right in range(len(new_s)):
+            char = new_s[right][1]
             
-        for right in range(size):
-            char = s[right]
-            
-            if char in new_s:
-                new_s[char] += 1
+            if char in window:
+                window[char] += 1
             else:
-                new_s[char] = 1
+                window[char] = 1
             
-            if char in new_t and new_s[char] == new_t[char] :
+            if window[char] == new_t[char] :
                     formed += 1                
                 
             while formed == len(new_t) and left <= right:
                 
-                char = s[left]
+                char = new_s[left][1]
+
+                start = new_s[left][0]
+                end = new_s[right][0]
+                if end-start+1 < res[0]:
+                    res[0] = end-start+1
+                    res[1] = start
+                    res[2] = end
                 
-                if right-left+1 < res[0]:
-                    res[0] = right-left+1
-                    res[1] = left
-                    res[2] = right
-                
-                new_s[char] -= 1
+                window[char] -= 1
                 left += 1
                 
-                if char in new_t and new_s[char] < new_t[char]:
+                if window[char] < new_t[char]:
                     formed -= 1
+                    
                     
         return '' if res[0] == float('inf') else s[res[1]:res[2]+1]
 
